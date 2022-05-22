4937e1a36e287f5a84dcc95a3ee924439fff19c9
vitassablino <105976325+vitassablino@users.noreply.github.com>
2022-05-22 14:01:29 +0300

    Добавьте файлы проекта.

---
+++ Task 5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string firstName = "Чебышев";
+        string lastName = "Александр";
+        string buffer;
+
+        Console.WriteLine("До замены");
+        Console.WriteLine("Имя : " + firstName);
+        Console.WriteLine("Фамилия : " + lastName);
+        buffer = firstName;
+        firstName = lastName;
+        lastName = buffer;
+        Console.WriteLine("После замены");
+        Console.WriteLine("Имя : " + firstName);
+        Console.WriteLine("Фамилия : " + lastName);
+    }
+}
